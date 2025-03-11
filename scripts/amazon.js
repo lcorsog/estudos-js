44d2fab93da1f3f906a4b7022fc61ee021a84497
luis corsog <lcorsog@gmail.com>
2025-03-11 14:15:02 -0300

    criação de uma função para a edição da moeda

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { products } from "../data/products.js";
 import { cart, addToCart } from "../data/cart.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productsHTML = "";
 
@@ -27,9 +28,7 @@ products.forEach((product) => {
         }</div>
         </div>
 
-        <div class="product-price">${(product.priceCents / 100).toFixed(
-          2
-        )}</div>
+        <div class="product-price">$${formatCurrency(product.priceCents)}</div>
 
         <div class="product-quantity-container">
         <select>
