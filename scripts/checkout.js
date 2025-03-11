a828fe824337fef3e2d3385b3625e2d8bd893019
luis corsog <lcorsog@gmail.com>
2025-03-11 15:38:41 -0300

    criação da função de remove, para retirar o item da tela

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,8 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  productsHTML += `<div class="cart-item-container">
+  productsHTML += `
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -92,6 +93,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
