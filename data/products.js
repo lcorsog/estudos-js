b0c7086ce58a0d7f7aa404138ef26ea5a717c1ad
luis corsog <lcorsog@gmail.com>
2025-03-12 16:35:41 -0300

    utilizando herança e polymorfismo para adicionar a tabela de tamanhos em objetos que forma criados como clothing

--- data/products.js
+++ data/products.js
@@ -33,6 +33,28 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return "";
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    // super chama o construtor da classe pai
+    super(productDetails);
+    this.sizeCartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size Chart
+        </a>
+    `;
+  }
 }
 
 const product1 = new Product({
@@ -519,6 +541,9 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
